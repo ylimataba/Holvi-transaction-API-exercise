ec841a76f1d55e657e3b54f1011f0099be820b59
Matti Yli-Ojanperä <yliojanperamatti@gmail.com>
2018-11-21 14:54:31 +0200

    test for fetching the account balance

--- fintech/tests.py
+++ fintech/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from django.contrib.auth.models import User
+from fintech.models import Account
 
-# Create your tests here.
+class BalanceTestCase(APITestCase):
+    def test_get_account_balance(self):
+        user1 = User.objects.create_user(username='user1', password='12345')
+        user1.save()
+        account = Account.objects.create(name='test account 1', balance=100, user=user1)
+        account.save()
+        response = self.client.get('/accounts/' + str(account.uuid) + '/balance')
+        self.assertEqual(response.data, {'balance': 100})

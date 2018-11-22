e33c8bc978cbd2e9f93d726ee62c94c9f7acc72c
Matti Yli-Ojanperä <yliojanperamatti@gmail.com>
2018-11-22 10:04:03 +0200

    tests for POST new transactions to account also fixed fetching endpoint to /transactions/?account=account_uuid/

--- fintech/tests.py
+++ fintech/tests.py
@@ -1,22 +1,26 @@
+from django.urls import reverse
+from rest_framework import status
 from rest_framework.test import APITestCase
 from django.contrib.auth.models import User
 from fintech.models import Account, Transaction
 
 class GetTestCase(APITestCase):
-    def test_get_account_balance(self):
+    account = Account.objects.none
+    def setUp(self):
         user1 = User.objects.create_user(username='user1', password='12345')
         user1.save()
         account = Account.objects.create(name='test account 1', balance=100, user=user1)
         account.save()
-        response = self.client.get('/accounts/' + str(account.uuid) + '/balance')
+        self.account = account
+
+    def test_get_account_balance(self):
+        uuid = self.account.uuid
+        response = self.client.get('/accounts/' + str(uuid) + '/balance')
         self.assertEqual(response.data, {'balance': 100})
 
     def test_get_account_transactions(self):
-        user1 = User.objects.create_user(username='user1', password='12345')
-        user1.save()
-        account = Account.objects.create(name='test account 1', balance=100, user=user1)
-        account.save()
         date = "2018-11-21"
+        account = self.account
         transaction1 = Transaction.objects.create(
                 account=account,
                 transaction_date=date,
@@ -38,5 +42,49 @@ class GetTestCase(APITestCase):
         transaction1.save()
         transaction2.save()
         transaction3.save()
-        response = self.client.get('/accounts/' + str(account.uuid) + '/transactions/')
+        response = self.client.get('/transactions/?account=' + str(account.uuid))
         self.assertEqual(len(response.data), 2)
+
+class PostTestCase(APITestCase):
+    account = Account.objects.none
+    url = reverse('transaction-list')
+    data = {
+        'transaction_date': "2018-11-21",
+        'description':'Test',
+        'active':True
+    }
+    def setUp(self):
+        user1 = User.objects.create_user(username='user1', password='12345')
+        user1.save()
+        account = Account.objects.create(name='test account 1', balance=100, user=user1)
+        account.save()
+        self.account = account
+        self.data['account'] = self.account.uuid
+
+    def test_positive_transaction(self):
+        self.data['amount'] = 20
+        response = self.client.post(self.url, self.data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        account = Account.objects.get(uuid=response.data['account'])
+        self.assertEqual(account.balance, 120)
+
+    def test_negative_transaction(self):
+        self.data['amount'] = -20
+        response = self.client.post(self.url, self.data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        account = Account.objects.get(uuid=response.data['account'])
+        self.assertEqual(account.balance, 80)
+
+    def test_zero_balance_transaction(self):
+        self.data['amount'] = -100
+        response = self.client.post(self.url, self.data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        account = Account.objects.get(uuid=response.data['account'])
+        self.assertEqual(account.balance, 0)
+
+    def test_negative_balance_transaction(self):
+        self.data['amount'] = -101
+        response = self.client.post(self.url, self.data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        account = Account.objects.get(uuid=self.account.uuid)
+        self.assertEqual(account.balance, 100)
